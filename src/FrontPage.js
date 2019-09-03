3d0f70d5db539f03f2f23c425a17160bd02000d9
rowneee <roni.shabo@gmail.com>
2019-09-03 10:36:47 -0400

    bg

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -3,8 +3,18 @@ import React from 'react'
 class FrontPage extends React.Component {
 
   render() {
+    let imgUrl = '/Users/ronishabo/Flatiron/Portfolio/portfolio-frontend/public/purple.png'
+    const style = {
+      width: '100%',
+      height: '800px',
+      backgroundColor: '#ffbd7a',
+      backgroundSize: 'cover'
+    };
     return (
       <div>
+        <div class="bg-component" style={style} >
+          <h1 align="center">Portfolio</h1>
+        </div>
       </div>
     )
   }
